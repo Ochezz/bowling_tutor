03ebb13c0fd1aa018b21e78cc7ec41b9ce5388e7
Lim <cogns523@gmail.co.jp>
2021-07-01 15:16:56 +0900

    FeedScreenレイアウト修正

--- Screen/FeedScreen.js
+++ Screen/FeedScreen.js
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { View, Text, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
+//Screen import
+import Camera from '../Component/Camera.js';
+// import { RNCamera } from 'react-native-camera';
+
 //css import
 import '../CSS/FeedScreen.css'
 
@@ -10,6 +14,15 @@ import BowlingImage from '../Image/BowlingImage.png';
 import LogoImage from '../Image/logo.png';
 
 function FeedScreen() {
+    // const callCamera = () => {
+    //     <View>
+    //     <RNCamera
+    //         style={{width: 200, height: 200}}
+    //         type={RNCamera.Constants.Type.back}
+    //         captureAudio={false}
+    //     />
+    // </View>
+    // }
     return (
         <View>
             {/* <Text>FeedScreen</Text> */}
@@ -20,15 +33,16 @@ function FeedScreen() {
             <div id='video-div'>
                 <div id='video-preImg-div'>
                     <img id='preImg' src={BowlingImage}/>
-                    <Icon id='aaa' name='chevron-left' size={35}/>
+                    <Icon name='chevron-left' size={35} onClick={()=>console.log('pre video')}/>
                 </div>
                 <div id='video-nowImg-div'>
                     <img id='nowImg' src={BowlingImage}/>
-                    <Icon name='dots-horizontal-circle' size={40} color={"#D0D0D0"}/>
+                    <Icon name='dots-horizontal-circle' size={40} color={"#D0D0D0"} onClick={()=>console.log('now video')}/>
                 </div>
                 <div id='video-nextImg-div'>
                     <img id='nextImg' src={BowlingImage}/>
-                    <Icon name="chevron-right" size={35}/>
+                    <Icon name='chevron-right' size={35} onClick={()=>console.log('next video')}/>
+                    {/* <Icon name="chevron-right" size={35} onClick={()=>callCamera}/> */}
                 </div>
             </div>
             <div id='info-div'>
@@ -42,7 +56,7 @@ function FeedScreen() {
             </div>
             <div id='record-div'>
                 <button id='record-btn'>
-                    <Icon name="camera" color='white' size={40}/>
+                    <Icon name="camera" color='white' size={40} onClick={()=>console.log('camera')}/>
                 </button>
             </div>
         </View>
